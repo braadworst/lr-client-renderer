ced82f58ed50f02c8aa0f17fbaae4c0bf1b77798
braadworst <royniels83@gmail.com>
2017-04-13 20:45:35 +0800

    Made a better version of the client renderer

--- index.js
+++ index.js
@@ -5,39 +5,43 @@ module.exports = update => {
   function addObserver(id) {
     const observer = new MutationObserver(function(mutations) {
       mutations.forEach(function(mutation) {
-        if (mutation.attributeName === 'lg-placeholder') {
-          update({ matchValue : window.location.pathname, updateType : 'domReady' });
-        }
+        mutation.addedNodes.forEach(node => {
+          if (node.getAttribute && node.getAttribute('data-lr')) {
+            if (!node.getAttribute('id')) {
+              throw new Error('Could not create an update event for component, id is missing: ', node);
+            }
+            const updateType = node.getAttribute('id') + node.getAttribute('data-lr').slice(0, 1).toUpperCase() + node.getAttribute('data-lr').slice(1);
+            update({ matchValue : window.location.pathname, updateType });
+          }
+        });
       });
     });
 
     // pass in the target node, as well as the observer options
-    observer.observe(document.querySelector(id), { attributes : true, childList : false, subtree : false });
-    placeholders[id].observer = observer;
+    observer.observe(document.querySelector(id), { attributes : false, childList : true, subtree : false });
+    placeholders[id] = observer;
   }
 
   const exposed = {
     render(html, id) {
-      if (placeholders[id] && placeholders[id].observer) {
-        placeholders[id].observer.disconnect();
+      if (!placeholders[id]) {
+        addObserver(id);
       }
-      placeholders[id] = { html };
-    },
-    html() {
-      Object
-        .keys(placeholders)
-        .filter(id => placeholders[id].html)
-        .forEach(id => {
-          if (!document.querySelector(id)) {
-            throw new Error(`Trying to render, but could not find placeholder ${ id }`);
-          }
-          addObserver(id);
-          document.querySelector(id).innerHTML = placeholders[id].html;
-          document.querySelector(id).setAttribute('lg-placeholder', id);
-          delete placeholders[id].html;
-        });
+      if (!document.querySelector(id)) {
+        throw new Error(`Trying to render, but could not find placeholder ${ id }`);
+      }
+      document.querySelector(id).innerHTML = html;
     }
-  }
+  };
+
+  document.addEventListener('DOMContentLoaded', () => {
+    [].slice.call(document.querySelectorAll('[data-lr="loaded"]')).forEach(component => {
+      if (!component.getAttribute('id')) {
+        throw new Error('Found component, but id is missing: ', component);
+      }
+      update({ matchValue : window.location.pathname, updateType : component.getAttribute('id') + 'Loaded' });
+    });
+  });
 
   return exposed;
 };
